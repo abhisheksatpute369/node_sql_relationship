65b3636a2ab0711aad6699042a93384a1377333e
Abhishek Satpute <abhisheksatpute99@gmail.com>
2022-08-14 14:46:11 +0530

    database connected

---
+++ server/src/config/db.js
@@ -0,0 +1,10 @@
+// const mysql = require('mysql');
+const {Sequelize} = require('sequelize');
+
+const sequalize = new Sequelize('relations', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+}
+);
+  
+module.exports = sequalize;
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const body_parser = require("body-parser");
-
 require("dotenv").config();
+const connectdb = require("./config/db")
 
 const app = express()
 
@@ -10,8 +10,15 @@ app.use(express.json());
 app.use(cors());
 app.use(body_parser.urlencoded ({extended: false}));
 
-const PORT = process.env.PORT || 3003;
 
-app.listen(PORT,()=>{
+
+const PORT = process.env.PORT || 3003;
+app.listen(PORT, ()=>{
+    connectdb.authenticate().then(() => {
+        console.log('Yep! you connected to database');
+    }).catch(err => {
+        console.error('Sorry! unable to connect', err);
+    }
+    );
     console.log(`server runing on port${PORT}`)
 })
