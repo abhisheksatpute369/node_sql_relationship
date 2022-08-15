58b06f8dcbd34ddc2b29d0969eda62aba8c0c57a
Abhishek Satpute <abhisheksatpute99@gmail.com>
2022-08-15 22:57:08 +0530

    one to many done

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,6 +2,7 @@
 const {Sequelize, DataTypes} = require('sequelize');
 const Company = require("../models/company.model");
 const Compdetails = require("../models/compdetail.model");
+const Vehicle = require("../models/vehicle.model")
 
 const sequalize = new Sequelize('relations', 'root', '', {
     host: 'localhost',
@@ -11,12 +12,19 @@ const sequalize = new Sequelize('relations', 'root', '', {
 
 const db = {};
 
+// putting models into db object 
 db.sequalize = sequalize;
 db.Company = Company(sequalize, DataTypes);
 db.Compdetails = Compdetails(sequalize,DataTypes);
+db.Vehicle = Vehicle(sequalize, DataTypes);
 
+// one to one 1:1 relationship 
 db.Company.hasOne(db.Compdetails, {foreignKey: 'comp_id'});
 db.Compdetails.belongsTo(db.Company, {foreignKey: 'comp_id'});
+
+//one to many relationship 
+db.Company.hasMany(db.Vehicle, {foreignKey: 'comp_id'});
+db.Vehicle.belongsTo(db.Company, {foreignKey: 'comp_id'});
 
 db.sequalize.sync({force:false}).then(() => {
     console.log(`synchronized to data base`);
