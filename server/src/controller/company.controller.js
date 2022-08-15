0a24d9d8d0d949d66ed0c114fca420590bc2d953
Abhishek Satpute <abhisheksatpute99@gmail.com>
2022-08-15 22:05:17 +0530

    add route for get singlr company

--- server/src/controller/company.controller.js
+++ server/src/controller/company.controller.js
@@ -3,6 +3,43 @@ const express = require('express');
 const Router = express.Router();
 const {Company  , Compdetails} = require("../config/db");
 
+
+//route for get company data
+Router.get('/', (req, res) => {
+    Company.findAll({
+        include:[{
+            model:Compdetails,
+            attributes:['compdetail_id' , 'ceo_name','manager','headquarter','employee_num']
+     }]
+    }).then(Companys => {
+        res.send(Companys);
+    }
+    ).catch(err => {
+        res.send(err);
+    }
+    );
+});
+
+//route for get perticular company data
+Router.get('/:comp_id', (req, res) => {
+    // var id = req.params.comp_id
+    Company.findAll({
+        where:{
+            comp_id:req.params.comp_id
+        },
+        include:[{
+            model:Compdetails,
+            attributes:['compdetail_id' , 'ceo_name','manager','headquarter','employee_num']
+     }]
+    }).then(Companys => {
+        res.send(Companys);
+    }
+    ).catch(err => {
+        res.send(err);
+    }
+    );
+});
+
 // route for post the company data 
 Router.post('/addcompany', (req, res) => {
     Company.create(req.body).then(Company => {
