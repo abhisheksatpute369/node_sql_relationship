e5c973a28ed67854af5329bffda05f9a6874e1dd
Abhishek Satpute <abhisheksatpute99@gmail.com>
2022-08-17 12:02:47 +0530

    fetch vehicles using dealers id

--- cliant/src/components/bikes.js
+++ cliant/src/components/bikes.js
@@ -5,22 +5,35 @@ const Bikes = () => {
 
     const [bikes, setbikes] = useState([]);
     const [dealers, setdealers] =useState([]);
+    const [vehicles, setvehicles] = useState([]);
     const [show, setshow] = useState("");
     const {id} = useParams()
 
+    // for getting all bikes of that perticular company 
     const getsingle = async ()=>{
         const data = await fetch(`http://localhost:3030/vehicle/${id}`);
         const result = await (data.json());
         setbikes(result);
     }
 
+    // for geting dealers for perticular bike 
     const getalldealers = async (id, name) => {
         // Navigate(`/dealer/${id}`)
         document.getElementById("dealerstable").style.visibility = "visible";
-        var data = await fetch(`http://localhost:3030/dealer/${id}`);
-        var result = await (data.json());
-        setdealers(result[0].dealers);
+        const ddata = await fetch(`http://localhost:3030/dealer/${id}`);
+        const dresult = await (ddata.json());
+        setdealers(dresult[0].dealers);
         setshow(name);
+    }
+
+    // for getting vehicles for perticular dealer 
+    const getallvehicle = async (id, name) => {
+        document.getElementById("bikestable").style.visibility = "visible";
+        const vdata = await fetch(`http://localhost:3030/vehicles/${id}`);
+        // console.log(vdata);
+        const vresult = await (vdata.json());
+        setvehicles(vresult[0]?.bikes);
+        
     }
 
     useEffect(()=>{
@@ -70,6 +83,7 @@ const Bikes = () => {
                         <th>Phone</th>
                         <th>Email</th>
                         <th>Address</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -82,7 +96,7 @@ const Bikes = () => {
                                 <td>{ele?.d_phone}</td>
                                 <td>{ele?.d_email}</td>
                                 <td>{ele?.d_address}</td>
-                                {/* <td onClick={()=>getalldealers(ele.v_id)} style={{cursor:'pointer'}}>Dealers</td> */}
+                                <td onClick={()=>getallvehicle(ele?.d_id, ele?.d_name)} style={{cursor:'pointer'}}>Bikes</td>
                             </tr>
                         )
                     })
@@ -90,6 +104,39 @@ const Bikes = () => {
                 </tbody>
             </table>
         </div>
+
+         {/* table for showing vehicle list for that dealer  */}
+         <div id="bikestable">
+            <h4 id="vtitle">bikes</h4>
+            <table>
+                <thead id="heading">
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Milage</th>
+                        <th>Launchyear</th>
+                        {/* <th>Action</th> */}
+                    </tr>
+                </thead>
+                <tbody>
+                {
+                    vehicles.map((ele)=>{
+                        return(
+                            <tr key={ele?.v_id} id="rows">
+                                <td>{ele?.v_id}</td>
+                                <td>{ele?.v_name}</td>
+                                <td>{ele?.v_milage}</td>
+                                <td>{ele?.v_launchyear}</td>
+                                {/* <td>{ele?.v_address}</td> */}
+                                {/* <td onClick={()=>getallvehicle(ele.v_id)} style={{cursor:'pointer'}}>Dealers</td> */}
+                            </tr>
+                        )
+                    })
+                }
+                </tbody>
+            </table>
+        </div>
+
         </div>
     );
 };
