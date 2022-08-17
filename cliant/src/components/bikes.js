0c6dc6eea8a612d98ea87e3bb67c096c789cc314
Abhishek Satpute <abhisheksatpute99@gmail.com>
2022-08-17 12:23:09 +0530

    css styling

--- cliant/src/components/bikes.js
+++ cliant/src/components/bikes.js
@@ -7,6 +7,7 @@ const Bikes = () => {
     const [dealers, setdealers] =useState([]);
     const [vehicles, setvehicles] = useState([]);
     const [show, setshow] = useState("");
+    const [see, setsee] = useState("");
     const {id} = useParams()
 
     // for getting all bikes of that perticular company 
@@ -33,6 +34,7 @@ const Bikes = () => {
         // console.log(vdata);
         const vresult = await (vdata.json());
         setvehicles(vresult[0]?.bikes);
+        setsee(name)
         
     }
 
@@ -71,72 +73,72 @@ const Bikes = () => {
                 </tbody>
             </table>
         </div>
+        <div id ="maindata">
+            {/* list for showing dealers list for that vehicle  */}
+            <div id="dealerstable">
+                <h4 id="vtitle">Dealers for {show}</h4>
+                <table>
+                    <thead id="heading">
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Phone</th>
+                            <th>Email</th>
+                            <th>Address</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        dealers.map((ele)=>{
+                            return(
+                                <tr key={ele?.d_id} id="rows">
+                                    <td>{ele?.d_id}</td>
+                                    <td>{ele?.d_name}</td>
+                                    <td>{ele?.d_phone}</td>
+                                    <td>{ele?.d_email}</td>
+                                    <td>{ele?.d_address}</td>
+                                    <td onClick={()=>getallvehicle(ele?.d_id, ele?.d_name)} style={{cursor:'pointer'}}>Bikes</td>
+                                </tr>
+                            )
+                        })
+                    }
+                    </tbody>
+                </table>
+            </div>
 
-         {/* list for showing dealers list for that vehicle  */}
-        <div id="dealerstable">
-            <h4 id="vtitle">Dealers for {show}</h4>
-            <table>
-                <thead id="heading">
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Phone</th>
-                        <th>Email</th>
-                        <th>Address</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    dealers.map((ele)=>{
-                        return(
-                            <tr key={ele?.d_id} id="rows">
-                                <td>{ele?.d_id}</td>
-                                <td>{ele?.d_name}</td>
-                                <td>{ele?.d_phone}</td>
-                                <td>{ele?.d_email}</td>
-                                <td>{ele?.d_address}</td>
-                                <td onClick={()=>getallvehicle(ele?.d_id, ele?.d_name)} style={{cursor:'pointer'}}>Bikes</td>
-                            </tr>
-                        )
-                    })
-                }
-                </tbody>
-            </table>
+            {/* table for showing vehicle list for that dealer  */}
+            <div id="bikestable">
+                <h4 id="vtitle">{see} deals with</h4>
+                <table>
+                    <thead id="heading">
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Milage</th>
+                            <th>Launchyear</th>
+                            {/* <th>Action</th> */}
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        vehicles.map((ele)=>{
+                            return(
+                                <tr key={ele?.v_id} id="rows">
+                                    <td>{ele?.v_id}</td>
+                                    <td>{ele?.v_name}</td>
+                                    <td>{ele?.v_milage}</td>
+                                    <td>{ele?.v_launchyear}</td>
+                                    {/* <td>{ele?.v_address}</td> */}
+                                    {/* <td onClick={()=>getallvehicle(ele.v_id)} style={{cursor:'pointer'}}>Dealers</td> */}
+                                </tr>
+                            )
+                        })
+                    }
+                    </tbody>
+                </table>
+            </div>
         </div>
-
-         {/* table for showing vehicle list for that dealer  */}
-         <div id="bikestable">
-            <h4 id="vtitle">bikes</h4>
-            <table>
-                <thead id="heading">
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Milage</th>
-                        <th>Launchyear</th>
-                        {/* <th>Action</th> */}
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    vehicles.map((ele)=>{
-                        return(
-                            <tr key={ele?.v_id} id="rows">
-                                <td>{ele?.v_id}</td>
-                                <td>{ele?.v_name}</td>
-                                <td>{ele?.v_milage}</td>
-                                <td>{ele?.v_launchyear}</td>
-                                {/* <td>{ele?.v_address}</td> */}
-                                {/* <td onClick={()=>getallvehicle(ele.v_id)} style={{cursor:'pointer'}}>Dealers</td> */}
-                            </tr>
-                        )
-                    })
-                }
-                </tbody>
-            </table>
-        </div>
-
         </div>
     );
 };
